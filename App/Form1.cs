827f7e778fcb398766431a74b65c44e49c7d82c3
Anton <60883514+mythter@users.noreply.github.com>
2022-11-13 23:04:11 +0200

    Group choice auto resize added

--- App/Form1.cs
+++ App/Form1.cs
@@ -2,6 +2,9 @@ namespace App
 {
     public partial class Form1 : Form
     {
+        private Rectangle GroupChoiceOriginalRect;
+        private Size formOriginalSize;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,9 +25,9 @@ namespace App
             // vScrollBar1
             // 
             this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.vScrollBar1.Location = new System.Drawing.Point(1886, 0);
+            this.vScrollBar1.Location = new System.Drawing.Point(1466, 0);
             this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(16, 1033);
+            this.vScrollBar1.Size = new System.Drawing.Size(16, 953);
             this.vScrollBar1.TabIndex = 1;
             this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
             // 
@@ -36,9 +39,9 @@ namespace App
             "525-â",
             "525-á",
             "525"});
-            this.GroupChoice.Location = new System.Drawing.Point(470, 0);
+            this.GroupChoice.Location = new System.Drawing.Point(260, 0);
             this.GroupChoice.Name = "GroupChoice";
-            this.GroupChoice.Size = new System.Drawing.Size(450, 30);
+            this.GroupChoice.Size = new System.Drawing.Size(450, 28);
             this.GroupChoice.TabIndex = 2;
             this.GroupChoice.SelectedIndexChanged += new System.EventHandler(this.GroupChoice_SelectedIndexChanged);
             // 
@@ -55,7 +58,7 @@ namespace App
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.38889F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(467, 144);
             this.tableLayoutPanel1.TabIndex = 3;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint_1);
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // ChangePosition
             // 
@@ -71,16 +74,48 @@ namespace App
             // Form1
             // 
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.ClientSize = new System.Drawing.Size(1482, 953);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.GroupChoice);
             this.Controls.Add(this.vScrollBar1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.Load += Form1_Load;
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formOriginalSize = this.Size;
+            GroupChoiceOriginalRect = new Rectangle(GroupChoice.Location.X, GroupChoice.Location.Y, GroupChoice.Width, GroupChoice.Height);
+        }
+
+        private void resizeChildrenControls()
+        {
+            resizeControl(GroupChoiceOriginalRect, GroupChoice);
+        }
+
+        private void resizeControl(Rectangle originalControlRect, Control control)
+        {
+            float xRatio = (float)this.Width / (float)formOriginalSize.Width;
+            float yRatio = (float)this.Height / (float)formOriginalSize.Height;
+
+            int newX = (int)(originalControlRect.X * xRatio);
+            int newY = (int)(originalControlRect.Y * yRatio);
+
+            int newWidth = (int)(originalControlRect.Width * xRatio);
+            int newHeight = (int)(originalControlRect.Height * yRatio);
+
+            control.Location = new Point(newX, newY);
+            control.Size = new Size(newWidth, newHeight);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeChildrenControls();
         }
 
         private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
@@ -89,11 +124,6 @@ namespace App
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
         {
 
         }
